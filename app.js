67608fb377635cbfb32e7f189985dadadb204547
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2022-12-13 10:44:51 +0100

    sauvegarde avant installation de ionic

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const fs = require("fs")
+const axios = require('axios'); //  Axios est un client HTTP basé sur les promesses, compatible avec node.js et les navigateurs.
+const fs = require("fs").promises
 
 
 // chemin des router
@@ -26,25 +27,25 @@ app.use(routerUser, routerList, routerDetail)
 
 
 
-// Middleware pour gérer la page de contact
-app.post('/contact', (req, res) => {
+// // Middleware pour gérer la page de contact
+// app.post('/contact', (req, res) => {
   
-  try {
+//   try {
 
-    // Données du formulaire 
-    const { user_name, user_email, user_message } = req.body;
+//     // Données du formulaire 
+//     const { user_name, user_email, user_message } = req.body;
     
-     // On récupère les données du formulaire
-    const formContactMsg = { nom: user_name, email : user_email, message: user_message }
+//      // On récupère les données du formulaire
+//     const formContactMsg = { nom: user_name, email : user_email, message: user_message }
   
-    // Puis on les convertis en JSON
-    const formContactMsgJSON = JSON.stringify(formContactMsg);
+//     // Puis on les convertis en JSON
+//     const formContactMsgJSON = JSON.stringify(formContactMsg);
   
-    // Puis on stocke les données du formulaire dans un fichier JSON
-    fs.writeFileSync('formMsgContact.json', formContactMsgJSON);
+//     // Puis on stocke les données du formulaire dans un fichier JSON
+//     fs.writeFileSync('formMsgContact.json', formContactMsgJSON);
     
-    // Le code ici permet d'envoyer l'email 
-    return res.json(formContactMsgJSON)
+//     // Le code ici permet d'envoyer l'email 
+//     return res.json(formContactMsgJSON)
 
 
 
@@ -69,21 +70,48 @@ app.post('/contact', (req, res) => {
     // fs.writeFileSync('formMsgContact.json', newData);
   
   
-  } catch (err) {
-      console.error(err.message);
-      return res.status(400).json('Une erreur s\'est produite');
-  }
+//   } catch (err) {
+//       console.error(err.message);
+//       return res.status(400).json('Une erreur s\'est produite');
+//   }
+// });
+
+
+
+
+
+// Middleware pour recuperer et lire les messages de la page de contact à l'adresse : http://localhost:8080/messages
+//  pour lire les messages stocker dans msgContactForm.json 
+app.get('/messages', function (req, res) {
+ 
+fs.readFile('msgContactForm.json','utf8').then((fileContent) => {
+    console.log('coucou');
+    res.send(fileContent)
+});
+})
+
+// pour récupérer les données du formulaire à l'adresse /contact
+app.post('/contact', function(req, res) {
+
+  addUserInFile(req.body);
+  console.log(req.body);
+  res.send(req.body);
+
 });
 
 
 
 
 
-
-
-
-
-
 app.listen(port, () => {
-    console.log(`L'application est lancée sur le port : ${port}`);
-  });
+  console.log(`L'application est lancée sur le port : ${port}`);
+});
+
+
+function addUserInFile(message){
+    fs.readFile('msgContactForm.json','utf8').then((fileContent) => {
+        let messages = JSON.parse(fileContent);
+        messages.push(message);
+        fs.writeFile('msgContactForm.json', JSON.stringify(messages))
+    });
+}
