ae2c71df96e9d9f1132d2ab2643afd51c941d9f4
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2023-01-04 15:57:51 +0100

    new connexion DB

--- database.js
+++ database.js
@@ -3,11 +3,11 @@ const Pool = require("pg").Pool
 
 // On instancie une nouvelle pool avec le contenu de notre BDD
 const pool = new Pool({
-    user: "postgres",
-    password: "Digifab74",
-    host: "localhost",
+    user: "audcan",
+    password: "sVpITAVqE5KP96Spx7frO3BTyyzZy7rm",
+    host: "dpg-ceqoreha6gdovo1kkbtg-a.frankfurt-postgres.render.com",
     port: 5432,
-    database: "bdd_voyages"
+    database: "bdd_voyages_4eal"
 })
 
 // on exporte pour le mettre dans index-s.js
