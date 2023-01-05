a5de096948460d3315d7253b43704951e8fbcb75
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2023-01-05 10:11:16 +0100

    ajout ssl

--- database.js
+++ database.js
@@ -7,8 +7,17 @@ const pool = new Pool({
     password: "sVpITAVqE5KP96Spx7frO3BTyyzZy7rm",
     host: "dpg-ceqoreha6gdovo1kkbtg-a.frankfurt-postgres.render.com",
     port: 5432,
-    database: "bdd_voyages_4eal"
+    database: "bdd_voyages_4eal",
+    ssl:true
 })
 
 // on exporte pour le mettre dans index-s.js
 module.exports = pool
+
+
+
+// user: "postgres",
+// password: "Digifab74",
+// host: "localhost",
+// port: 5432,
+// database: "bdd_voyages"
