56fb2ab3f9e0160005920d9b77a33526e011f24d
Audrey Cannesson <103568338+AudreyCa@users.noreply.github.com>
2023-01-10 11:31:38 +0100

    maj database.js

--- database.js
+++ database.js
@@ -5,7 +5,6 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user: "audcan",
     password: process.env.BDD_password,
-    // password: "sVpITAVqE5KP96Spx7frO3BTyyzZy7rm",
     host: "dpg-ceqoreha6gdovo1kkbtg-a.frankfurt-postgres.render.com",
     port: 5432,
     database: "bdd_voyages_4eal",
